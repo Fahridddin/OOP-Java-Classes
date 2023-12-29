aed4298f8f07549b0b5fb3cb1e7f551ede425bcd
Name Surname <20221500282@newuzbekistanuniversity.uz>
2023-12-30 02:05:43 +0500

    OOP/Classes

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("StartUp:");
+        User Startup1 = new User();
+        Startup1.name = "Name:" + "UFeel";
+        Startup1.membership = "Owner:" + "Fahriddin";
+        User Startup2 = new User();
+        Startup2.name = "Name:" + "Eplant";
+        Startup2.membership = "Owner:" + "Fyodor";
+        User Startup3 = new User();
+        Startup3.name = "Name:" + "XFire";
+        Startup3.membership = "Owner:" + "Akmal";
+        User Startup4 = new User();
+        Startup4.name = "Name:" + "OyTickets";
+        Startup4.membership = "Owner:" + "Nurislom";
+        User Startup5 = new User();
+        Startup5.name = "Name:" + "MedBook";
+        Startup5.membership = "Owner:" + "Karimakhon";
+        User Startup6 = new User();
+        Startup6.name = "Name:" + "Flymart";
+        Startup6.membership = "Owner:" + "Alijon";
+        User Startup7 = new User();
+        Startup7.name = "Name:" + "Speaklish";
+        Startup7.membership = "Owner:" + "Bobirjon";
+        User Startup8 = new User();
+        Startup8.name = "Name:" + "Fashionista";
+        Startup8.membership = "Owner:" + "Nafisa";
+        User Startup9 = new User();
+        Startup9.name = "Name:" + "Steppeup";
+        Startup9.membership = "Owner:" + "Alisher";
+        User Startup10 = new User();
+        Startup10.name = "Name:" + "Biofuel";
+        Startup10.membership = "Owner:" + "Maxmud";
+        User Startup11 = new User();
+        Startup11.name = "Name:" + "Rent Market";
+        Startup11.membership = "Owner:" + "Akmaljon";
+        User Startup12 = new User();
+        Startup12.name = "Name:" + "Engline";
+        Startup12.membership = "Owner:" + "Shakhlo";
+        User Startup13 = new User();
+        Startup13.name = "Name:" + "Activo";
+        Startup13.membership = "Owner:" + "Gulasal";
+        User Startup14 = new User();
+        Startup14.name = "Name:" + "Rastik";
+        Startup14.membership = "Owner:" + "Shuxrat";
+
+        System.out.println(Startup1.name);
+        System.out.println(Startup1.membership);
+    }
+}
---
+++ User.java
@@ -0,0 +1,5 @@
+public class User {
+    String membership;
+    String name;
+}
+// capsulation
